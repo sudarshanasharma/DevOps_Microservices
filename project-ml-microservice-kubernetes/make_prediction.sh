42e49cdcbaf7f7f69fba68f028feba900a7a81af
sudarshanasharama <sudarshana.sharma7@gmail.com>
2020-12-13 16:52:17 -0800

    checking in working app

--- project-ml-microservice-kubernetes/make_prediction.sh
+++ project-ml-microservice-kubernetes/make_prediction.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 
-PORT=8000
-echo "Port: $PORT"
-
-# POST method predict
-curl -d '{  
-   "CHAS":{  
+PORT=8001
+curl -d '{
+   "CHAS":{
       "0":0
    },
-   "RM":{  
+   "RM":{
       "0":6.575
    },
-   "TAX":{  
+   "TAX":{
       "0":296.0
    },
-   "PTRATIO":{  
+   "PTRATIO":{
       "0":15.3
    },
-   "B":{  
+   "B":{
       "0":396.9
    },
-   "LSTAT":{  
+   "LSTAT":{
       "0":4.98
    }
 }'\
      -H "Content-Type: application/json" \
      -X POST http://localhost:$PORT/predict
+echo "Port: $PORT"
--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,11 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+sudo docker build --tag=final .
 # Step 2: 
 # List docker images
-
+sudo docker image ls
 # Step 3: 
 # Run flask app
+#sudo docker run -p 8001:81 -it final bash
+sudo docker run -p 8001:81 final
