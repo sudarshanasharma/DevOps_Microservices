98004185d4c458a80fc4faaa9a8c1e75157cfa66
sudarshanasharma <sudarshana.sharma7@gmail.com>
2020-12-15 19:57:35 +0000

    changing image tag to docker id/tag

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-sudo docker build --tag=final .
+docker build -t sudarshanas/udaproj5 .
 # Step 2: 
 # List docker images
-sudo docker image ls
+docker image ls
 # Step 3: 
 # Run flask app
 #sudo docker run -p 8001:81 -it final bash
-sudo docker run -p 8001:81 final
+docker run -p 8001:81 sudarshanas/udaproj5
