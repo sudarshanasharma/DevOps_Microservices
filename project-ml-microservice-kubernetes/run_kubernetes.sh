9b0b9f25ad09c3b6e960b90f86b483aa143c385a
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-12-15 10:31:39 -0800

    Update run_kubernetes.sh
    initial changes to script

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="sudarshanas/finalenv"
+sudo docker login --username "sudarshanas"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+sudo kubectl create deployment finalenv --image=$dockerpath
 # Step 3:
 # List kubernetes pods
-
+sudo kubectl get pods --all-namespaces
 # Step 4:
 # Forward the container port to a host
-
+sudo kubectl expose deployment finalenv --type="Nodeport" --port=3001
+sudo kubectl port-forward deployment/finalenv 3001:8001
