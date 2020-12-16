b1a9f115547bb0c298dcd99927af07f8e3010aa5
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-12-16 13:38:43 -0800

    Update upload_docker.sh
    put finishing touches to the script

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -3,15 +3,13 @@
 # Assumes that an image is built via `run_docker.sh`
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
-#final:latest
-dockerpath="c8cd1e3e3f8b"
+dockerpath=sudarshanas/udaproj5:latest
 # Step 2:
 # Authenticate & tag
 ##docker tag {image ID number} yourhubusername/{name_of_your_choice]:firsttry
 docker login --username "sudarshanas"
-docker tag c8cd1e3e3f8b sudarshanas/udaproj5:latest
+docker tag c8cd1e3e3f8b $dockerpath
 echo "Docker ID and Image: $dockerpath"
 # Step 3:docker push $dockerpath
 # Push image to a docker repository
-docker image push sudarshanas/udaproj5:latest
+docker image push $dockerpath
