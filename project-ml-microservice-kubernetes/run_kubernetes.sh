9a755338c14b7f6516cb3ff8fe7f8eee85e470f0
sudarshanasharma <sudarshana.sharma7@gmail.com>
2020-12-16 00:33:50 +0000

    Working script and output

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,15 +8,17 @@ dockerpath="sudarshanas/udaproj5"
 # Step 2
 # Run the Docker Hub container with kubernetes
 #kubectl create deployment udaproj5 --image="sudarshanas/udaproj5" --port=8001
-kubectl run udaproj5 --image="sudarshanas/udaproj5" --port=8001
+kubectl run udaproj5 --image=$dockerpath --port=81
+
+sleep 1m
 # Step 3:
 # List kubernetes pods
 kubectl get pods --all-namespaces
 # Step 4:
 # Forward the container port to a host
 #kubectl expose deployment udaproj5 --type="NodePort" --port=3001 --targetport=81
-kubectl expose pod/udaproj5 --type="NodePort" --port=3001 --target-port=81
+kubectl expose pod/udaproj5 --type="NodePort" --port=3001 --target-port=8001
 
 #kubectl port-forward deployment/udaproj5 3001:8001
-kubectl port-forward udaproj5 3001:8001
+kubectl port-forward udaproj5 3001:81
 
