e334f0e4d67f5eb39ce596f09fe890174dbd510a
Ubuntu <ubuntu@ip-172-31-2-233.us-west-2.compute.internal>
2020-12-17 03:23:12 +0000

    checkin in files after testing docker and kubernetes and linting

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -3,12 +3,14 @@
 # Assumes that an image is built via `run_docker.sh`
 # Step 1:
 # Create dockerpath
-dockerpath=sudarshanas/udaproj5:latest
+dockerpath="sudarshanas/udaproj5:latest"
+imageid="052d7c07f668"
+username="sudarshanas"
 # Step 2:
 # Authenticate & tag
-##docker tag {image ID number} yourhubusername/{name_of_your_choice]:firsttry
-docker login --username "sudarshanas"
-docker tag c8cd1e3e3f8b $dockerpath
+##docker tag {mage ID number} yourhubusername/{name_of_your_choice]:firsttry
+docker login --username $username
+docker tag $imageid $dockerpath
 echo "Docker ID and Image: $dockerpath"
 # Step 3:docker push $dockerpath
 # Push image to a docker repository
