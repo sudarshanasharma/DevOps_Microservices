d98e511d8b079c6a6f1383bbc3c5a9ec852d04af
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-12-17 18:40:04 -0800

    Update run_docker.sh
    Making a variable for docker image path

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-
+dockerpath="sudarshanas/udaproj5"
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t sudarshanas/udaproj5 .
+docker build -t $dockerpath .
 # Step 2: 
 # List docker images
 docker image ls
 # Step 3: 
 # Run flask app
 #sudo docker run -p 8001:81 -it final bash
-docker run -p 81:8000 sudarshanas/udaproj5
+docker run -p 81:8000 $dockerpath
